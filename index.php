a27431120224014b49409e6c1d1dc1a256b582a5
mrzep <ponybro734@gmail.com>
2020-04-23 23:46:35 +0300

    Лабораторная 9

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+if (file_exists(__DIR__."/vendor/autoload.php")) {
+    require __DIR__."/vendor/autoload.php";
+}
+
+use stolyarov\Log;
+use stolyarov\QuadraticEq;
+use stolyarov\StolyarovException;
+
+$eq=new QuadraticEq();
+
+$a=0;
+$b=0;
+$c=0;
+
+try {
+    function entercheck($num,$letter)
+    {
+        $pattern = '#^[0-9]*[.]?[0-9]+$#';
+        for (;;) {
+            $num=readline("Enter $letter=");
+            echo "\n";
+            if(preg_match($pattern,$num))
+            {
+                return $num;
+            }
+            else {
+                echo "Inappropriate symbols. Can only type numbers and dot\n";
+            }
+        }
+        return $num;
+    }
+
+    $a=entercheck($a,'a');
+    $b=entercheck($b,'b');
+    $c=entercheck($c,'c');
+
+    $eq->solve($a,$b,$c);
+} catch (StolyarovException $e) {
+    Log::log("Error: ".$e->getMessage());
+}
+
+Log::write();
